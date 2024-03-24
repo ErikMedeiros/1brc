b6897cc85cc07127ede199ed4803d1c623d6fcf9
Erik Medeiros <erikmedeiros4@gmail.com>
2024-03-23 22:20:40 -0300

    fix: memory leaks

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,20 @@
 const std = @import("std");
+const work = @import("work.zig").work;
 
-pub fn main() !void {}
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+    _ = args.skip();
+
+    const input_path = args.next().?;
+    const input_file = try std.fs.cwd().openFile(input_path, .{});
+    defer input_file.close();
+
+    var stdout_file = std.io.getStdOut();
+    defer stdout_file.close();
+
+    try work(input_file, &stdout_file, allocator);
+}
--- src/work.zig
+++ src/work.zig
@@ -5,6 +5,10 @@ pub fn work(input: std.fs.File, output_file: *std.fs.File, allocator: std.mem.Al
     var reader = br.reader();
 
     var map = std.StringArrayHashMap(StationInfo).init(allocator);
+    defer {
+        for (map.keys()) |key| allocator.free(key);
+        map.deinit();
+    }
 
     while (true) {
         var buf = std.ArrayList(u8).init(allocator);
@@ -20,21 +24,32 @@ pub fn work(input: std.fs.File, output_file: *std.fs.File, allocator: std.mem.Al
         const station_name = buf.items[0..pivot];
         const temp = try std.fmt.parseFloat(f16, buf.items[pivot + 1 ..]);
 
-        std.debug.print("{s}\n", .{station_name});
-        var entry = map.getPtr(station_name);
+        var entry = try map.getOrPut(station_name);
 
-        if (entry) |station| {
+        if (entry.found_existing) {
+            var station = entry.value_ptr;
             station.sum += temp;
             station.count += 1;
 
-            if (station.min > temp) station.min = temp;
-            if (station.max < temp) station.max = temp;
+            station.min = @min(station.min, temp);
+            station.max = @max(station.max, temp);
         } else {
-            try map.put(station_name, .{ .min = temp, .max = temp, .sum = temp, .count = 1 });
+            entry.key_ptr.* = try allocator.dupe(u8, station_name);
+            entry.value_ptr.* = .{ .min = temp, .max = temp, .sum = temp, .count = 1 };
         }
     }
 
-    map.sort(.{ .lessThan = lessThan });
+    const Ctx = struct {
+        items: [][]const u8,
+
+        pub fn lessThan(ctx: @This(), a_index: usize, b_index: usize) bool {
+            const a = ctx.items[a_index];
+            const b = ctx.items[b_index];
+            return std.mem.order(u8, a, b).compare(std.math.CompareOperator.lt);
+        }
+    };
+
+    map.sort(Ctx{ .items = map.keys() });
 
     var bw = std.io.bufferedWriter(output_file.writer());
     var writer = bw.writer();
@@ -44,19 +59,18 @@ pub fn work(input: std.fs.File, output_file: *std.fs.File, allocator: std.mem.Al
         const station = map.get(station_name).?;
         const average = @divFloor(station.sum, @as(f16, @floatFromInt(station.count)));
 
+        if (i != 0)
+            try std.fmt.format(writer, "{s}", .{", "});
+
         try std.fmt.format(
             writer,
-            "{s}{s}={d}/{d}/{d}",
-            .{ if (i == 0) "" else ", ", station_name, station.min, average, station.max },
+            "{s}={d:.1}/{d:.1}/{d:.1}",
+            .{ station_name, station.min, average, station.max },
         );
     }
 
     try writer.writeAll("}\n");
     try bw.flush();
-}
-
-fn lessThan(a_index: usize, b_index: usize) bool {
-    return std.mem.order(usize, &.{a_index}, &.{b_index}).compare(std.math.CompareOperator.lt);
 }
 
 const StationInfo = struct { min: f16, max: f16, sum: f32, count: usize };
